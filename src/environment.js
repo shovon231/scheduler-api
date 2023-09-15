92e9e3768f4dc706fdb4cd3ebf6afe2a1d40f69a
shovon231 <shovon231@gmail.com>
2023-09-15 06:40:49 -0400

    adding cypress for testing and configure the env file

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-const ENV = process.env.NODE_ENV || "development";
+const ENV = process.env.NODE_ENV || "test";
 const PATH = path.resolve(__dirname, "../.env." + ENV);
 
 require("dotenv").config({ path: PATH });
